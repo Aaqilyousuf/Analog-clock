cb04f170f63f958499ec76a348c53cdf963bc20d
Aaqil yousuf <118057216+Aaqilyousuf@users.noreply.github.com>
2023-09-26 21:19:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const hour=document.querySelector('.hrs');
+const minute=document.querySelector('.min');
+const second=document.querySelector('.sec');
+
+setInterval(runClock,1000);
+
+function runClock(){
+    const time = new Date;
+    const sec = time.getSeconds()/60;
+    const min = (sec+time.getMinutes())/60;
+    const hrs = (min+time.getHours())/12;
+    hour.style.setProperty("--needle",hrs*360);
+    minute.style.setProperty("--needle",min*360);
+    second.style.setProperty("--needle",sec*360);
+}
+
+runClock();
